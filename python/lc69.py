84f784022431f77cc3d4397282c09b08e52227cb
Rahul Katre <38406805+rahulgkatre@users.noreply.github.com>
2022-03-01 22:45:48 -0500

    Update lc69.py

--- python/lc69.py
+++ python/lc69.py
@@ -6,17 +6,14 @@ class Solution:
         l = 0
         r = x
         m = (l + r) // 2
-        
         while l < m and m < r:
             p = m * m
-
             if p < x:
                 l = m
             elif p > x:
                 r = m
             else:
                 break
-
             m = (l + r) // 2            
     
         return m
