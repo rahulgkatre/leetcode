f0e5e2a66775096f93d79b052a13233ca6f2ff17
Rahul Katre <38406805+rahulgkatre@users.noreply.github.com>
2022-03-01 22:55:01 -0500

    Update lc83.py

--- python/lc83.py
+++ python/lc83.py
@@ -3,11 +3,13 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr = head
         while curr and curr.next:
             if curr.val == curr.next.val:
+                # remove next node if same val
                 curr.next = curr.next.next
             else:
                 curr = curr.next
