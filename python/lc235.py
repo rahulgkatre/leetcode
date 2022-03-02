3330c635d05ac28382b75b0dbc2b08aec7dba7a4
Rahul Katre <38406805+rahulgkatre@users.noreply.github.com>
2022-03-01 22:47:07 -0500

    Update lc235.py

--- python/lc235.py
+++ python/lc235.py
@@ -4,19 +4,14 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-    
         if p.val < root.val and q.val < root.val:
             return self.lowestCommonAncestor(root.left, p, q)
-            
         elif p.val > root.val and q.val > root.val:
             return self.lowestCommonAncestor(root.right, p, q)
-            
         elif (p.val <= root.val and q.val > root.val) or \
              (p.val >= root.val and q.val < root.val) or \
              (p.val < root.val and q.val >= root.val) or \
              (p.val > root.val and q.val <= root.val):
-        return root
-        
+            return root
