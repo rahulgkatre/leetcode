08120d6682fc5fa507ceb0e9279757bdf04449c8
Rahul Katre <38406805+rahulgkatre@users.noreply.github.com>
2022-03-01 23:05:33 -0500

    Update lc235.py

--- python/lc235.py
+++ python/lc235.py
@@ -7,14 +7,13 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        curr = root
-        while curr:
-            if p.val < curr.val and q.val < curr.val:
+        while root:
+            if p.val < root.val and q.val < root.val:
                 # p, q, and LCA are in the left subtree
-                curr = curr.left
+                root = root.left
             elif p.val > curr.val and q.val > curr.val:
                 # p, q, and LCA are in the right subtree
-                curr = curr.right
+                root = root.right
             else:
                 # subtree containing both p and q has curr as root
-                return curr
+                return root
