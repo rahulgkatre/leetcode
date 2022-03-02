3e314dd11031aadb1cce4e2201a227961b27c2c8
Rahul Katre <38406805+rahulgkatre@users.noreply.github.com>
2022-03-01 22:54:05 -0500

    Update lc69.py

--- python/lc69.py
+++ python/lc69.py
@@ -1,11 +1,13 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        # edge case for 0 and 1
         if x in (0, 1):
             return x
         
+        # binary search
         l = 0
         r = x
-        m = (l + r) // 2
+        m = (l + r) // 2        
         while l < m and m < r:
             p = m * m
             if p < x:
@@ -15,5 +17,5 @@ class Solution:
             else:
                 break
             m = (l + r) // 2            
-    
+        
         return m
